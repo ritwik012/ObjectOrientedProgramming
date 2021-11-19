27a9e59ddbb3b1072ba201198d4218442c1fef08
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-11-19 09:37:11 +0530

    [Rutwik]Added:InventoryJsonFile

---
+++ ObjectOrientedProgramming/InventoryModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOrientedPrograms
+{
+    class InventoryModel
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ ObjectOrientedProgramming/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ObjectOrientedPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome to Object Oriented Programs");
+                Console.WriteLine("Choose the Option : 1.JSON \n 2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        InventoryMain inv = new InventoryMain();
+                        inv.DisplayData(@"E:\Git\ObjectOrientedProgramming\ObjectOrientedProgramming\Files\Inventory.json");
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
